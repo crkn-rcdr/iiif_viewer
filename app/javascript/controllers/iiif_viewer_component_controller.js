ae8ab9c28ba34f4b5fa952740e8b252b97194e54
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-09-26 19:55:34 -0400

    Random chance to load either UV or Mirador

--- app/javascript/controllers/iiif_viewer_component_controller.js
+++ app/javascript/controllers/iiif_viewer_component_controller.js
@@ -3,14 +3,47 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   initialize() {
     const documentId = this.element.getAttribute("data-docid")
-    const data = {
-      manifest: "https://www.canadiana.ca/iiif/"+documentId+"/manifest",
-      embedded: true // needed for codesandbox frame,
-    };
+    let canvasIndex = 0
     const params = new URLSearchParams(window.location.search)
-    if(params.has("pageNum")) data.canvasIndex = parseInt(params.get("pageNum")-1)
-    let viewer = UV.init("uv", data);
-    console.log(viewer)
-    //if(params.has("pageNum")) viewer.triggerSocket('uv.onCanvasIndexChanged', parseInt(params.get("pageNum")))
+    if(params.has("pageNum")) canvasIndex = parseInt(params.get("pageNum")-1)
+    if(Math.random() < 0.5) {
+      const data = {
+        manifest: "https://www.canadiana.ca/iiif/"+documentId+"/manifest",
+        embedded: true // needed for codesandbox frame,
+      };
+      data.canvasIndex = canvasIndex
+      document.getElementById("uv").style = "height: 40rem;";
+      let viewer = UV.init("uv", data);
+    } else {
+      document.getElementById("mirador").style = "width: 100%; height: 40rem; position: relative;";
+      let miradorInstance = Mirador.viewer({
+        id: 'mirador', // id selector where Mirador should be instantiated
+        //selectedTheme: 'dark', // dark also available
+        view: "catalogueView",
+        window: {
+            allowClose: false, // Prevent the user from closing this window
+            defaultSideBarPanel: 'info',
+            sideBarOpenByDefault: true,
+            defaultView: 'single',
+            views: [ // Only allow the user to select single and gallery view
+              { key: 'single', behaviors: ['individuals', 'paged'] },
+              { key: 'book', behaviors: ['paged'] },
+              { key: 'scroll', behaviors: ['continuous'] },
+              { key: 'gallery' },
+            ]
+        },
+        windows: [{
+          manifestId: "https://www.canadiana.ca/iiif/"+documentId+"/manifest",
+          //view: 'single',
+          canvasIndex,
+        }],
+        workspace: {
+            type: 'mosaic',
+        },
+        workspaceControlPanel: {
+            enabled: false, // Remove extra workspace settings
+        },
+      });
+    }
   }
 }
