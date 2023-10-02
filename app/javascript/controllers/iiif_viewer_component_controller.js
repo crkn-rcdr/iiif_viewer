713f380b61363800a1d989853c5a5f3537529269
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-10-02 17:50:48 -0400

    Add clover viewer

--- app/javascript/controllers/iiif_viewer_component_controller.js
+++ app/javascript/controllers/iiif_viewer_component_controller.js
@@ -6,7 +6,10 @@ export default class extends Controller {
     let canvasIndex = 0
     const params = new URLSearchParams(window.location.search)
     if(params.has("pageNum")) canvasIndex = parseInt(params.get("pageNum")-1)
-    if(Math.random() < 0.5) {
+    let choice = Math.random()
+    if(choice < 0.3) {
+      let clovers = document.getElementsByTagName("clover-viewer")
+      clovers[0].style="display:none;"
       //https://codesandbox.io/s/uv-config-example-7kh4s?file=/uv-config.json
       const data = {
         manifest: "https://www.canadiana.ca/iiif/"+documentId+"/manifest",
@@ -15,7 +18,9 @@ export default class extends Controller {
       data.canvasIndex = canvasIndex
       document.getElementById("viewer-name").innerHTML = "Universal Viewer."
       let viewer = UV.init("page-viewer", data);
-    } else {
+    } else if(choice < 0.6) {
+      let clovers = document.getElementsByTagName("clover-viewer")
+      clovers[0].style="display:none;"
       document.getElementById("viewer-name").innerHTML = "Mirador."
       //https://github.com/ProjectMirador/mirador/blob/master/src/config/settings.js
       let miradorInstance = Mirador.viewer({
@@ -48,6 +53,12 @@ export default class extends Controller {
             enabled: false, // Remove extra workspace settings
         },
       });
+    } else {
+      document.getElementById("page-viewer").style="display:none;"
+      document.getElementById("viewer-name").innerHTML = "Clover."
+      let clovers = document.getElementsByTagName("clover-viewer")
+      clovers[0].setAttribute("id", "https://www.canadiana.ca/iiif/"+documentId+"/manifest") // id="https://api.dc.library.northwestern.edu/api/v2/works/8a833741-74a8-40dc-bd1d-c416a3b1bb38?as=iiif"
     }
+
   }
 }
